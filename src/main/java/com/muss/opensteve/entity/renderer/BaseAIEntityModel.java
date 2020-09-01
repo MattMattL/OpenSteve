846bd1db2b9258035910492a8571393faa0f2c69
Matt L <primemuon@gmail.com>
2020-09-01 23:41:05 +0100

    Fix entity rendering issues

--- src/main/java/com/muss/opensteve/entity/renderer/BaseAIEntityModel.java
+++ src/main/java/com/muss/opensteve/entity/renderer/BaseAIEntityModel.java
@@ -21,7 +21,127 @@ import java.util.List;
 import java.util.Random;
 import java.util.function.Function;
 
-public class BaseAIEntityModel<T extends BaseAIEntity> extends EntityModel<T>
+public class BaseAIEntityModel<T extends LivingEntity> extends BipedModel<T>
+{
+	private List<ModelRenderer> modelRenderers = Lists.newArrayList();
+
+	public final boolean smallArms;
+
+	public final ModelRenderer bipedHead;
+	public final ModelRenderer bipedLeftArm;
+	public final ModelRenderer bipedLeftLeg;
+	public final ModelRenderer bipedBody;
+
+	public final ModelRenderer bipedHeadwear;
+	public final ModelRenderer bipedLeftArmwear;
+	public final ModelRenderer bipedRightArmwear;
+	public final ModelRenderer bipedLeftLegwear;
+	public final ModelRenderer bipedRightLegwear;
+	public final ModelRenderer bipedBodyWear;
+
+	public BaseAIEntityModel(float modelSize, boolean smallArmsIn)
+	{
+		super(RenderType::getEntityTranslucent, modelSize, 0.0F, 64, 64);
+		this.smallArms = smallArmsIn;
+
+		this.bipedHead = new ModelRenderer(this, 0, 0);
+		this.bipedHead.addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F);
+		this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
+
+		this.bipedHeadwear = new ModelRenderer(this, 32, 0);
+		this.bipedHeadwear.addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.5F);
+		this.bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
+
+		this.bipedBody = new ModelRenderer(this, 16, 16);
+		this.bipedBody.addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F);
+		this.bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
+
+		if (smallArmsIn)
+		{
+			this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+			this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize);
+			this.bipedLeftArm.setRotationPoint(5.0F, 2.5F, 0.0F);
+
+			this.bipedRightArm = new ModelRenderer(this, 40, 16);
+			this.bipedRightArm.addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize);
+			this.bipedRightArm.setRotationPoint(-5.0F, 2.5F, 0.0F);
+
+			this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+			this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize + 0.25F);
+			this.bipedLeftArmwear.setRotationPoint(5.0F, 2.5F, 0.0F);
+
+			this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+			this.bipedRightArmwear.addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, modelSize + 0.25F);
+			this.bipedRightArmwear.setRotationPoint(-5.0F, 2.5F, 10.0F);
+		}
+		else
+		{
+			this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+			this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize);
+			this.bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+
+			this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+			this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+			this.bipedLeftArmwear.setRotationPoint(5.0F, 2.0F, 0.0F);
+
+			this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+			this.bipedRightArmwear.addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+			this.bipedRightArmwear.setRotationPoint(-5.0F, 2.0F, 10.0F);
+		}
+
+		this.bipedLeftLeg = new ModelRenderer(this, 16, 48);
+		this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize);
+		this.bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+
+		this.bipedLeftLegwear = new ModelRenderer(this, 0, 48);
+		this.bipedLeftLegwear.addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+		this.bipedLeftLegwear.setRotationPoint(1.9F, 12.0F, 0.0F);
+
+		this.bipedRightLegwear = new ModelRenderer(this, 0, 32);
+		this.bipedRightLegwear.addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, modelSize + 0.25F);
+		this.bipedRightLegwear.setRotationPoint(-1.9F, 12.0F, 0.0F);
+
+		this.bipedBodyWear = new ModelRenderer(this, 16, 32);
+		this.bipedBodyWear.addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, modelSize + 0.25F);
+		this.bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
+	}
+
+
+	@Override
+	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch)
+	{
+		super.setRotationAngles(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+		this.bipedLeftLegwear.copyModelAngles(this.bipedLeftLeg);
+		this.bipedRightLegwear.copyModelAngles(this.bipedRightLeg);
+		this.bipedLeftArmwear.copyModelAngles(this.bipedLeftArm);
+		this.bipedRightArmwear.copyModelAngles(this.bipedRightArm);
+		this.bipedBodyWear.copyModelAngles(this.bipedBody);
+
+	}
+
+	public void setVisible(boolean visible)
+	{
+		super.setVisible(visible);
+		this.bipedLeftArmwear.showModel = visible;
+		this.bipedRightArmwear.showModel = visible;
+		this.bipedLeftLegwear.showModel = visible;
+		this.bipedRightLegwear.showModel = visible;
+		this.bipedBodyWear.showModel = visible;
+	}
+
+	/*@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha)
+	{
+		bipedHead.render(matrixStack, buffer, packedLight, packedOverlay);
+		bipedLeftArm.render(matrixStack, buffer, packedLight, packedOverlay);
+		bipedRightArm.render(matrixStack, buffer, packedLight, packedOverlay);
+		bipedLeftLeg.render(matrixStack, buffer, packedLight, packedOverlay);
+		bipedRightLeg.render(matrixStack, buffer, packedLight, packedOverlay);
+		bipedBody.render(matrixStack, buffer, packedLight, packedOverlay);
+	}*/
+}
+
+/*public class BaseAIEntityModel<T extends BaseAIEntity> extends EntityModel<T>
 {
 	private List<ModelRenderer> modelRenderers = Lists.newArrayList();
 
@@ -130,5 +250,5 @@ public class BaseAIEntityModel<T extends BaseAIEntity> extends EntityModel<T>
 		bipedRightLeg.render(matrixStack, buffer, packedLight, packedOverlay);
 		bipedBody.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
-}
+}*/
 
