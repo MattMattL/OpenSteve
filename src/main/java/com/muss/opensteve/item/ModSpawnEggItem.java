a9f2081d1be63b1e43a1d505d12fd54cb71bf3dd
Matt L <primemuon@gmail.com>
2020-09-01 15:48:39 +0100

    Fix spawn egg textures

--- src/main/java/com/muss/opensteve/item/ModSpawnEggItem.java
+++ src/main/java/com/muss/opensteve/item/ModSpawnEggItem.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.util.Lazy;
@@ -18,49 +19,94 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ModSpawnEggItem extends net.minecraft.item.SpawnEggItem
+public class ModSpawnEggItem extends SpawnEggItem
 {
-	protected static final List<ModSpawnEggItem> UNADDED_EGGS = new ArrayList<>();
-	private final Lazy<? extends EntityType<?>> entityTypeSupplier;
+		protected static final List<ModSpawnEggItem> UNADDED_EGGS = new ArrayList<>();
+		private final Lazy<? extends EntityType<?>> entityTypeSupplier;
 
-	public ModSpawnEggItem(final RegistryObject<? extends EntityType<?>> entityTypeSupplier, int primaryColorIn, int secondaryColorIn, Item.Properties builder) {
-		super(null, primaryColorIn, secondaryColorIn, builder);
-
-		this.entityTypeSupplier = Lazy.of(entityTypeSupplier::get);
-
-		UNADDED_EGGS.add(this);
-	}
-
-	public static void initSpawnEggs()
-	{
-		final Map<EntityType<?>, net.minecraft.item.SpawnEggItem> EGGS = ObfuscationReflectionHelper.getPrivateValue(net.minecraft.item.SpawnEggItem.class, null, "field_195987_b");
-
-		DefaultDispenseItemBehavior dispenseBehaviour = new DefaultDispenseItemBehavior()
+		public ModSpawnEggItem(final RegistryObject<? extends EntityType<?>> entityTypeSupplier, final int primaryColour, final int secondaryColour, final Item.Properties properties)
 		{
-			@Override
-			protected ItemStack dispenseStack(IBlockSource source, ItemStack stack)
-			{
-				Direction direction = source.getBlockState().get(DispenserBlock.FACING);
-				EntityType<?> type = ((net.minecraft.item.SpawnEggItem) stack.getItem()).getType(stack.getTag());
-				type.spawn(source.getWorld(), stack, null, source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
-				stack.shrink(1);
-
-				return stack;
-			}
-		};
-
-		for (final net.minecraft.item.SpawnEggItem spawnEgg : UNADDED_EGGS)
-		{
-			EGGS.put(spawnEgg.getType(null), spawnEgg);
-			DispenserBlock.registerDispenseBehavior(spawnEgg, dispenseBehaviour);
+			super(null, primaryColour, secondaryColour, properties);
+			this.entityTypeSupplier = Lazy.of(entityTypeSupplier::get);
+			UNADDED_EGGS.add(this);
 		}
 
-		UNADDED_EGGS.clear();
-	}
+		public static void initSpawnEggs()
+		{
+			final Map<EntityType<?>, SpawnEggItem> EGGS = ObfuscationReflectionHelper.getPrivateValue(SpawnEggItem.class, null, "field_195987_b");
 
-	@Override
-	public EntityType<?> getType(@Nullable CompoundNBT compound)
-	{
-		return this.entityTypeSupplier.get();
-	}
+			DefaultDispenseItemBehavior dispenseBehaviour = new DefaultDispenseItemBehavior()
+			{
+				@Override
+				protected ItemStack dispenseStack(IBlockSource source, ItemStack stack)
+				{
+					Direction direction = source.getBlockState().get(DispenserBlock.FACING);
+					EntityType<?> type = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
+					type.spawn(source.getWorld(), stack, null, source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
+					stack.shrink(1);
+
+					return stack;
+				}
+			};
+
+			for (final SpawnEggItem spawnEgg : UNADDED_EGGS)
+			{
+				EGGS.put(spawnEgg.getType(null), spawnEgg);
+				DispenserBlock.registerDispenseBehavior(spawnEgg, dispenseBehaviour);
+			}
+
+			UNADDED_EGGS.clear();
+		}
+
+		@Override
+		public EntityType<?> getType(CompoundNBT compound)
+		{
+			return this.entityTypeSupplier.get();
+		}
 }
+
+//{
+//	protected static final List<ModSpawnEggItem> UNADDED_EGGS = new ArrayList<>();
+//	private final Lazy<? extends EntityType<?>> entityTypeSupplier;
+//
+//	public ModSpawnEggItem(final RegistryObject<? extends EntityType<?>> entityTypeSupplier, int primaryColorIn, int secondaryColorIn, Item.Properties builder) {
+//		super(null, primaryColorIn, secondaryColorIn, builder);
+//
+//		this.entityTypeSupplier = Lazy.of(entityTypeSupplier::get);
+//
+//		UNADDED_EGGS.add(this);
+//	}
+//
+//	public static void initSpawnEggs()
+//	{
+//		final Map<EntityType<?>, SpawnEggItem> EGGS = ObfuscationReflectionHelper.getPrivateValue(SpawnEggItem.class, null, "field_195987_b");
+//
+//		DefaultDispenseItemBehavior dispenseBehaviour = new DefaultDispenseItemBehavior()
+//		{
+//			@Override
+//			protected ItemStack dispenseStack(IBlockSource source, ItemStack stack)
+//			{
+//				Direction direction = source.getBlockState().get(DispenserBlock.FACING);
+//				EntityType<?> type = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
+//				type.spawn(source.getWorld(), stack, null, source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
+//				stack.shrink(1);
+//
+//				return stack;
+//			}
+//		};
+//
+//		for (final SpawnEggItem spawnEgg : UNADDED_EGGS)
+//		{
+//			EGGS.put(spawnEgg.getType(null), spawnEgg);
+//			DispenserBlock.registerDispenseBehavior(spawnEgg, dispenseBehaviour);
+//		}
+//
+//		UNADDED_EGGS.clear();
+//	}
+//
+//	@Override
+//	public EntityType<?> getType(@Nullable CompoundNBT compound)
+//	{
+//		return this.entityTypeSupplier.get();
+//	}
+//}
