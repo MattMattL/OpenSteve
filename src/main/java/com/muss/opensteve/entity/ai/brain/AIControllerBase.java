bd4148b0060ec72d63c706df78a76c24388916b6
Matt L <primemuon@gmail.com>
2020-09-06 20:29:55 +0100

    Add abstract methods in AIControllerBase

--- src/main/java/com/muss/opensteve/entity/ai/brain/AIControllerBase.java
+++ src/main/java/com/muss/opensteve/entity/ai/brain/AIControllerBase.java
@@ -1,13 +1,41 @@
 package com.muss.opensteve.entity.ai.brain;
 
+import com.muss.opensteve.entity.monster.BaseAIEntity;
 import net.minecraft.nbt.CompoundNBT;
 
-public class AIControllerBase extends DeepNNetBase
+public abstract class AIControllerBase
 {
-	public AIControllerBase(int netIn, int netOut, int netDepth)
+	protected DeepNNetBase deepNNet;
+	protected BaseAIEntity entity;
+
+	public AIControllerBase(BaseAIEntity entityIn, int netIn, int netOut, int netDepth)
 	{
-		super(netIn, netOut, netDepth);
+		this.entity = entityIn;
+		this.deepNNet = new DeepNNetBase(netIn, netOut, netDepth);
 	}
+
+	public int getMaxNNetOutput()
+	{
+		int iMax = 0;
+
+		for(int i=1; i<this.deepNNet.NET_OUT; i++)
+		{
+			if(this.deepNNet.vectorOut[i] > this.deepNNet.vectorOut[iMax])
+				iMax = i;
+		}
+
+		return iMax;
+	}
+
+	/* put entity's environmental factors as NNet inputs */
+	public abstract void setNNetInput();
+
+	/* execute tasks according to NNet outputs */
+	public abstract void runEntityBehavior();
+
+	/* evaluate the outcome and run back propagations */
+	public abstract void fixEntityBehavior();
+
 
 	public void writeAdditional(CompoundNBT compound)
 	{
