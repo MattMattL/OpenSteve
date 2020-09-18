d70c4a23295e4a376aa1e375c281605f86b37e84
Matt L <primemuon@gmail.com>
2020-09-18 10:12:31 +0100

    Add a helper command /opensteve

--- src/main/java/com/muss/opensteve/command/OpenSteveCommands.java
+++ src/main/java/com/muss/opensteve/command/OpenSteveCommands.java
@@ -2,8 +2,11 @@ package com.muss.opensteve.command;
 
 import com.muss.opensteve.OpenSteve;
 import com.muss.opensteve.command.impl.GreetingCommand;
+import com.muss.opensteve.command.impl.KillCommand;
 import com.muss.opensteve.command.impl.PrintCommand;
+import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,11 +15,22 @@ public class OpenSteveCommands
 {
 	public static void registerCommands(RegisterCommandsEvent event)
 	{
+		event.getDispatcher()
+				.register(Commands.literal("opensteve").requires(context -> { return context.hasPermissionLevel(2); })
+						.executes(source -> { return instructions(source.getSource()); })
+				);
+
         event.getDispatcher()
-				.register(Commands.literal("creator").requires(context -> {return context.hasPermissionLevel(2);})
+				.register(Commands.literal("creator").requires(context -> { return context.hasPermissionLevel(2); })
 						.then(PrintCommand.register())
 						.then(GreetingCommand.register())
 						/* add commands here */
 				);
     }
+
+    private static int instructions(CommandSource source)
+	{
+		source.sendFeedback(new TranslationTextComponent("commands.opst.opensteve"), false);
+		return 1;
+	}
 }
