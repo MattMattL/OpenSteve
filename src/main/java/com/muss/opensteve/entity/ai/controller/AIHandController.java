b1fb58604f5ac5e92324bbdc4d77e1e0a83051f4
Matt L <primemuon@gmail.com>
2020-09-20 15:50:29 +0100

    Add AI hand and inventory controller class

---
+++ src/main/java/com/muss/opensteve/entity/ai/controller/AIHandController.java
@@ -0,0 +1,8 @@
+package com.muss.opensteve.entity.ai.controller;
+
+import com.muss.opensteve.entity.ai.brain.AIControllerBase;
+import com.muss.opensteve.entity.monster.BaseAIEntity;
+
+public class AIHandController
+{
+}
