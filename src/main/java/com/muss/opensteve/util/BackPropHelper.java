8e4260c7cf5ebeb2a31a3bc37ebfeea6f4997c24
Matt L <primemuon@gmail.com>
2020-10-02 21:57:39 +0100

    Add BackPropHelper

---
+++ src/main/java/com/muss/opensteve/util/BackPropHelper.java
@@ -0,0 +1,14 @@
+package com.muss.opensteve.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BackPropHelper
+{
+	private List<Factor> factorList = new ArrayList<>();
+
+	public class Factor<T>
+	{
+		T data;
+	}
+}
