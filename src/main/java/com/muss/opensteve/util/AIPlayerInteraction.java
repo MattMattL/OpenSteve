e570fb6d6d2046be190ae2902df1149fa07d4fdd
Matt L <primemuon@gmail.com>
2020-10-11 11:13:58 +0100

    Add chest block interaction that prints inventory

--- src/main/java/com/muss/opensteve/util/AIPlayerInteraction.java
+++ src/main/java/com/muss/opensteve/util/AIPlayerInteraction.java
@@ -5,11 +5,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class AIPlayerInteraction
 {
@@ -18,6 +16,9 @@ public class AIPlayerInteraction
 		if(player.getHeldItemMainhand().getItem() == Items.STICK)
 			return AIPlayerInteraction.onStickAction(entity);
 
+		if(player.getHeldItemMainhand().getItem() == Items.CHEST)
+			return AIPlayerInteraction.onChestAction(entity, player);
+
 		return ActionResultType.PASS;
 	}
 
@@ -25,7 +26,22 @@ public class AIPlayerInteraction
 	{
 		entity.setDead();
 		entity.remove();
-		entity.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 0.2F, ((entity.world.rand.nextFloat() - entity.world.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
+		entity.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 0.2F, ((entity.world.rand.nextFloat()-entity.world.rand.nextFloat())*0.7F+1.0F)*2.0F);
+
+		return ActionResultType.SUCCESS;
+	}
+
+	private static ActionResultType onChestAction(BaseAIEntity entity, PlayerEntity player)
+	{
+		if(!entity.shouldReceiveFeedback())
+			return ActionResultType.PASS;
+
+		for(ItemStack itemStack : entity.inventory.mainInventory)
+		{
+			String formatted = String.format("%3d %s", itemStack.getCount(), itemStack.getItem().getName().getString());
+
+			player.sendMessage(new TranslationTextComponent("interaction.opst.onChestAction", formatted), Util.field_240973_b_);
+		}
 
 		return ActionResultType.SUCCESS;
 	}
