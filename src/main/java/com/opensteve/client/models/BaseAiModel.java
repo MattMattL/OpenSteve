d6c1928faba4b62605bc112c1fdfd84ca3d58984
Matt L <primemuon@gmail.com>
2022-10-30 01:51:06 +0000

    Fix model to extend HumanoidModel

--- src/main/java/com/opensteve/client/models/BaseAiModel.java
+++ src/main/java/com/opensteve/client/models/BaseAiModel.java
@@ -3,6 +3,7 @@ package com.opensteve.client.models;
 import com.opensteve.OpenSteve;
 import com.opensteve.entity.BaseAiEntity;
 import net.minecraft.client.model.*;
+import net.minecraft.client.model.geom.LayerDefinitions;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
@@ -12,33 +13,22 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class BaseAiModel extends PigModel<BaseAiEntity>
+public class BaseAiModel extends HumanoidModel<BaseAiEntity>
 {
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(OpenSteve.MODID, "base_ai"), "main");
 
     public BaseAiModel(ModelPart modelPart)
     {
-      super(modelPart);
+        super(modelPart);
     }
 
 
     public static LayerDefinition createBodyLayer()
     {
-        return createBodyLayer(new CubeDeformation(0.0F));
+//        return createBodyLayer(new CubeDeformation(0.0F));
 
-//        MeshDefinition meshDefinition = createBodyMesh(CubeDeformation.NONE);
-//        PartDefinition partDefinition = meshDefinition.getRoot();
-//
-//        partDefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 13).addBox(-3.0F, -11.0F, -2.0F, 6.0F, 5.0F, 7.0F, CubeDeformation.NONE), PartPose.ZERO);
-//
-//        return LayerDefinition.create(meshDefinition, 64, 64);
-    }
+        MeshDefinition mesh = HumanoidModel.createMesh(new CubeDeformation(0f), 0f);
 
-    public static MeshDefinition createBodyMesh(CubeDeformation p_170670_)
-    {
-       MeshDefinition meshdefinition = new MeshDefinition();
-       PartDefinition partdefinition = meshdefinition.getRoot();
-
-       return meshdefinition;
+        return LayerDefinition.create(mesh, 0, 0);
     }
 }
