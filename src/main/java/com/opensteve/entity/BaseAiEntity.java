6b2fb2ff98e8c3eb31b8583442ee11feeaeb2202
Matt L <primemuon@gmail.com>
2022-10-30 02:15:25 +0000

    Add required methods
    - This includes getArmorSlots, getItemBySlot
    - This fixes the bug where it cannot be hit
    - Still no texture

--- src/main/java/com/opensteve/entity/BaseAiEntity.java
+++ src/main/java/com/opensteve/entity/BaseAiEntity.java
@@ -4,25 +4,79 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
-public class BaseAiEntity extends Pig
+import java.util.Iterator;
+import java.util.Random;
+
+public class BaseAiEntity extends LivingEntity
 {
-	public BaseAiEntity(EntityType<? extends Pig> type, Level level)
+	public BaseAiEntity(EntityType<? extends LivingEntity> type, Level level)
 	{
 		super(type, level);
+	}
+
+
+
+
+	@Override
+	public Iterable<ItemStack> getArmorSlots()
+	{
+		// TODO: return proper armour slots
+
+		return new Iterable<ItemStack>()
+		{
+			@NotNull
+			@Override
+			public Iterator<ItemStack> iterator()
+			{
+				return new Iterator<ItemStack>()
+				{
+					@Override
+					public boolean hasNext()
+					{
+						return false;
+					}
+
+					@Override
+					public ItemStack next()
+					{
+						return ItemStack.EMPTY;
+					}
+				};
+			}
+		};
+	}
+
+	@Override
+	public ItemStack getItemBySlot(EquipmentSlot slot)
+	{
+		// TODO: return proper item
+
+		return ItemStack.EMPTY;
+	}
+
+	@Override
+	public void setItemSlot(EquipmentSlot slot, ItemStack item)
+	{
+
 	}
 
 	public static AttributeSupplier.Builder getBaseAiAttributes()
 	{
 		return Mob.createMobAttributes()
-				.add(Attributes.MAX_HEALTH, 20.0)
-				.add(Attributes.ARMOR, 0.0);
+				.add(Attributes.MAX_HEALTH, 20.0D)
+				.add(Attributes.ARMOR, 0.0D)
+				.add(Attributes.MOVEMENT_SPEED, 1.0D);
 	}
 
 	@Override
-	public void tick()
+	public HumanoidArm getMainArm()
 	{
-		super.tick();
+		Random random = new Random();
+
+		return (random.nextInt(100) >= 90)? HumanoidArm.RIGHT : HumanoidArm.LEFT;
 	}
 }
